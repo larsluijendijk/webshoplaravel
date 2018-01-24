6ff837f070178d78077f2d62a3138dbfb28963c3
Lars Luijendijk <larsluijendijk@gmail.com>
2018-01-24 13:23:49 +0100

    rollback

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,12 +9,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/products/index', 'ProductsController@index')->name('productsindex');
+Route::get('/products/index', 'ProductsController@index')->name('productsindex')->middleware('auth');
